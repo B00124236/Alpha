d81a76ead7f41e3a70e90849be38f3d919a7bd6e
B00124236 <71829587+B00124236@users.noreply.github.com>
2020-11-28 09:10:05 +0000

    improved player code

---
+++ Alpha game version/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float pace = 10.0f;
+    private float zBound = 4;
+    private Rigidbody playersRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playersRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+
+        ConstrainPlayersPosition();
+    }
+
+    //Player is moved by arrow keys back, forward, left to right visa versa
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playersRb.AddForce(Vector3.forward * pace * verticalInput);
+        playersRb.AddForce(Vector3.right * pace * horizontalInput);
+    }
+
+    //Keeps player in certain space with invisable walls
+    void ConstrainPlayersPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+}
